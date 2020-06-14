997b905c5525eb0a0a1266f5d79091b46f056fff
Paul Krznaric <paulkrznaric@gmail.com>
2020-06-14 14:44:07 -0400

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Paul Krznaric on 2020-06-14.
+//  Copyright © 2020 Krznarnetic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
